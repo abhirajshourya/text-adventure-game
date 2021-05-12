d752e7056a67830f01ad161a38d64ffa582e31fa
Abhiraj Shourya <abhirajshourya@gmail.com>
2021-05-13 00:32:59 +0530

    Update AdventureGame.cs

--- Text Adventure Game/Assets/AdventureGame.cs
+++ Text Adventure Game/Assets/AdventureGame.cs
@@ -28,10 +28,19 @@ public class AdventureGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        for (int index=0; index < nextStates.Length; index++)
         {
-            state = nextStates[0];
+                if(Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
         }
+
+        /*
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                state = nextStates[0];
+            }
         else if(Input.GetKeyDown(KeyCode.Alpha2))
         {
             state = nextStates[1];
@@ -40,6 +49,7 @@ public class AdventureGame : MonoBehaviour
         {
             state = nextStates[2];
         }
+        */
         textComponent.text=state.GetStateStory();
     }
 
